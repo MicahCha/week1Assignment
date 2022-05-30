5375ac4e5a48000323ee25c872739ca3c3b19774
Micah Chaney <micahvonchaney@gmail.com>
2022-05-30 14:21:07 -0500

    2nd commit for week lab

---
+++ Week1Lab/VariablesAndOperations.js
@@ -0,0 +1,43 @@
+//create a variable to hold the quantity of available plane seats left on a flight
+let availablePlaneSeats = 6;
+		
+//create a variable to hold the cost of groceries at checkout
+let costOfGroceries = 23.64;
+
+//create a variable to hold a person's middle initial
+let middleInitial = 'S';
+
+//create a variable to hold true if it's hot outside and false if it's cold outside
+let isHotOutside = false;
+
+//create a variable to hold a customer's first name
+let customerFirstName = 'Micah';
+
+//create a variable to hold a street address
+let streetAddress = "W Bel Air";
+
+//print all variables to the console
+console.log(availablePlaneSeats);
+console.log(costOfGroceries);
+console.log (middleInitial);
+console.log(isHotOutside);
+console.log (customerFirstName);
+console.log (streetAddress);
+
+//a customer booked 2 plane seats, remove 2 seats from the available seats variable
+
+
+//impulse candy bar purchase, add 2.15 to the grocery total
+//costOfGroceries = costOfGroceries + 2.15;
+
+
+//birth certificate was printed incorrectly, change the middle initial to something else
+
+
+//the season has changed, update the hot outside variable to be opposite of what it was
+
+
+//create a new variable called full name using the customer's first name, the middle initial, and a last name of your choice
+
+
+//print a line to the console that introduces the customer and says they live at the address variable
